f7456b2fe657e366512de78221bf7502d25a3804
wu2305 <73875310+wu2305@users.noreply.github.com>
2026-07-12 13:08:42 +0800

    test(runtime): reject inactive and duplicate generations

--- tests/cpp/runtime_stop_coordinator_test.cpp
+++ tests/cpp/runtime_stop_coordinator_test.cpp
@@ -5,9 +5,16 @@
 
 using ppp::app::runtime::RuntimeStopCoordinator;
 
+BOOST_AUTO_TEST_CASE(stop_is_rejected_before_generation_activation) {
+    RuntimeStopCoordinator stop;
+    BOOST_TEST(!stop.TryBeginStop(0));
+    BOOST_TEST(!stop.TryBeginStop(1));
+    BOOST_TEST(!stop.BeginGeneration(0));
+}
+
 BOOST_AUTO_TEST_CASE(stop_is_claimed_once_per_generation) {
     RuntimeStopCoordinator stop;
-    stop.BeginGeneration(7);
+    BOOST_REQUIRE(stop.BeginGeneration(7));
 
     BOOST_TEST(stop.TryBeginStop(7));
     BOOST_TEST(!stop.TryBeginStop(7));
@@ -16,23 +23,35 @@ BOOST_AUTO_TEST_CASE(stop_is_claimed_once_per_generation) {
     stop.CompleteStop(7, true);
     BOOST_TEST(!stop.IsStopping(7));
     BOOST_TEST(stop.IsCompleted(7));
+    BOOST_TEST(stop.WasCleanupSuccessful(7));
+    BOOST_TEST(!stop.TryBeginStop(7));
+}
+
+BOOST_AUTO_TEST_CASE(duplicate_generation_cannot_reopen_completed_stop) {
+    RuntimeStopCoordinator stop;
+    BOOST_REQUIRE(stop.BeginGeneration(7));
+    BOOST_REQUIRE(stop.TryBeginStop(7));
+    stop.CompleteStop(7, true);
+
+    BOOST_TEST(!stop.BeginGeneration(7));
+    BOOST_TEST(stop.IsCompleted(7));
     BOOST_TEST(!stop.TryBeginStop(7));
 }
 
 BOOST_AUTO_TEST_CASE(old_generation_cannot_stop_new_session) {
     RuntimeStopCoordinator stop;
-    stop.BeginGeneration(7);
+    BOOST_REQUIRE(stop.BeginGeneration(7));
     BOOST_REQUIRE(stop.TryBeginStop(7));
     stop.CompleteStop(7, true);
 
-    stop.BeginGeneration(8);
+    BOOST_REQUIRE(stop.BeginGeneration(8));
     BOOST_TEST(!stop.TryBeginStop(7));
     BOOST_TEST(stop.TryBeginStop(8));
 }
 
 BOOST_AUTO_TEST_CASE(stale_completion_does_not_complete_current_generation) {
     RuntimeStopCoordinator stop;
-    stop.BeginGeneration(8);
+    BOOST_REQUIRE(stop.BeginGeneration(8));
     BOOST_REQUIRE(stop.TryBeginStop(8));
 
     stop.CompleteStop(7, true);
@@ -41,4 +60,5 @@ BOOST_AUTO_TEST_CASE(stale_completion_does_not_complete_current_generation) {
 
     stop.CompleteStop(8, false);
     BOOST_TEST(stop.IsCompleted(8));
+    BOOST_TEST(!stop.WasCleanupSuccessful(8));
 }
